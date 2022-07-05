15f2f305a26c6a3f739f3e657933e4d1a10b131a
Anastasia <bonnyn7@yandex.ru>
2022-07-05 16:39:20 +0300

    Added Task2

---
+++ ex2/Task2.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+namespace Homework4
+{
+    class Task2
+    {
+      public static void Main(string[] args)
+      {
+        Console.WriteLine ("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int SumNumber(int number)
+        {
+          int counter = Convert.ToString(number).Length;
+          int prior = 0;
+          int result = 0;
+            for (int i = 0; i < counter; i++)
+            {
+             prior = number - number % 10;
+             result = result + (number - prior);
+             number = number / 10;
+            }
+          return result;
+        }
+        int sumNumber = SumNumber(number);
+        Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+      }
+    }
+}
